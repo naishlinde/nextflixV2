9f0399590280d1b35f5f1c8fb0155cf7c13ed391
naishlinde <ilinde@ucsc.edu>
2021-03-05 22:05:35 -0500

    -

--- js/titlepage.js
+++ js/titlepage.js
@@ -3,27 +3,3 @@
  * Date: 3/2/2021
  * Title: Next -> Flix
 **/
-
-/**
-
-unogsNG NextGen Netflix Global Search API
-
-link:
-https://rapidapi.com/unogs/api/unogsng?endpoint=apiendpoint_6f0b642e-ee36-43c5-8a53-30aa86d9a8ff
-
-**/
-
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://unogsng.p.rapidapi.com/search?type=movie&limit=1&subtitle=english&countrylist=78&audio=english",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "75315872b0msh9c9e2b899bec1c0p1a8451jsnbb30815cd0ab",
-		"x-rapidapi-host": "unogsng.p.rapidapi.com"
-	}
-};
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
